91882d3797b49006ceda84a65e4db43dfdd076ed
LindaKorsak <lindakorsak007@gmail.com>
2024-05-28 15:16:42 +0200

    Oculus
    The project was created by me based on one of the tutorials by the author @vadimprokapchuk (https://www.youtube.com/@vadymprokopchuk)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const tabItem = document.querySelectorAll(".tabs-btn-item");
+const tabContent = document.querySelectorAll(".tabs-content-item");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs-btn-item-active");
+  });
+
+  tabTarget.classList.add("tabs-btn-item-active");
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs-content-item-active");
+}
